d9fc7fe86c4fcf8547a7821b703aeb0988526081
Nana-Muthuswamy <narayanan_eie@yahoo.com>
2017-02-17 21:54:26 -0800

    Renamed the view controller class and re-organized the project file.

--- TipCalc/TipViewController.swift
+++ TipCalc/TipViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TipViewController.swift
 //  TipCalc
 //
 //  Created by Nana on 2/16/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TipViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
