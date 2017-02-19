4f8f381e124f05da661a700d0cf4e25225faad9c
Nana-Muthuswamy <narayanan_eie@yahoo.com>
2017-02-18 16:21:14 -0800

    Hooked up IBOutlets and IBAction.

--- TipCalc/TipViewController.swift
+++ TipCalc/TipViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class TipViewController: UIViewController {
 
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipType: UISegmentedControl!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    @IBOutlet weak var totalType: UISegmentedControl!
+
+    @IBOutlet weak var totalFor2: UILabel!
+    @IBOutlet weak var totalFor3: UILabel!
+    @IBOutlet weak var totalFor4: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,9 @@ class TipViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func billAmountChanged(_ sender: UITextField) {
+        print("Bill: \(sender.text!)")
+    }
 
 }
 
